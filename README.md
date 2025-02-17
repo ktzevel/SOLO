8ac1c167cabe4c3eec5fbf255293f3a252d225d3
ktzevelekaki <ktzevelekaki@ee.ethz.ch>
2025-02-17 17:06:46 +0100

    website: added content, sliders and images.

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
-# SOLO
-Sun Off, Lights On: Photorealistic Monocular Nighttime Simulation for Robust Semantic Perception
+# Sun Off, Lights On (SOLO): Photorealistic Monocular Nighttime Simulation for Robust Semantic Perception
+
+__WACV 2025 (Oral, Best Paper Award Candidate)__
+
+This repository represents the official implementation of the paper titled "Sun Off, Lights On: Photorealistic Monocular Nighttime Simulation for Robust Semantic Perception".
 
 [![Website](docs/badges/badge-website.svg)](https://ktzevel.github.io/SOLO/)
 [![Paper](https://img.shields.io/badge/arXiv-PDF-b31b1b)](http://arxiv.org/abs/2407.20336)
 
-Konstantinos Tzevelekakis,
+[Konstantinos Tzevelekakis](https://scholar.google.com/citations?hl=de&user=8GEpNJYAAAAJ),
 [Shutong Zhang](https://scholar.google.com/citations?user=JYMjWq8AAAAJ&hl=el&oi=sra),
 [Luc Van Gool](https://scholar.google.com/citations?user=TwMib_QAAAAJ&hl=el&oi=sra),
 [Christos Sakaridis](https://people.ee.ethz.ch/~csakarid/)
+
+We present SOLO (Sun Off, Lights On), a monocular day-to-night translation method
+that can be used to translate a daytime semantic segmentation dataset into a nighttime one
+by converting each daytime image to its nighttime counterpart while preserving the original
+semantic segmentation annotations. To the best of our knowledge, SOLO is the first monocular
+physically based approach in 3D for simulating nighttime.
 
 ## 📢 News
 2024-11-19: Initial setup, authors and citation related information is released.<br>
--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,78 +1,46 @@
-window.HELP_IMPROVE_VIDEOJS = false;
-
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
+if (jQuery) {
+	// jQuery is loaded
+	console.log("available")
+  } else {
+	// jQuery is not loaded
+	console.log("not available")
   }
-}
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
+$(function(){
+	$("#day-to-night .twentytwenty-container").twentytwenty({
+	  default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+	  orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+	  before_label: 'Input', // Set a custom before label
+	  after_label: 'SOLO', // Set a custom after label
+	  no_overlay: false, //Do not show the overlay with before and after
+	  move_slider_on_hover: false, // Move slider on mouse hover?
+	  move_with_handle_only: false, // Allow a user to swipe anywhere on the image to control slider movement. 
+	  click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+	});
+  });
 
+$(function(){
+	$("#annotations .twentytwenty-container").twentytwenty({
+	  default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+	  orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+	  before_label: 'Input', // Set a custom before label
+	  after_label: 'Labels', // Set a custom after label
+	  no_overlay: false, //Do not show the overlay with before and after
+	  move_slider_on_hover: false, // Move slider on mouse hover?
+	  move_with_handle_only: false, // Allow a user to swipe anywhere on the image to control slider movement. 
+	  click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+	});
+  });
 
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-    });
-
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
-    bulmaSlider.attach();
-
-})
+$(function(){
+	$("#nighttime .twentytwenty-container").twentytwenty({
+	  default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+	  orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+	  before_label: 'Real', // Set a custom before label
+	  after_label: 'SOLO', // Set a custom after label
+	  no_overlay: false, //Do not show the overlay with before and after
+	  move_slider_on_hover: false, // Move slider on mouse hover?
+	  move_with_handle_only: false, // Allow a user to swipe anywhere on the image to control slider movement. 
+	  click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+	});
+  });
